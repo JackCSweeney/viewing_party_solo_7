23773f9dff438e1668d9cdcba4e723aed5915519
Jack Sweeney <jackcsweeney@gmail.com>
2024-03-20 13:12:07 -0700

    test: Write test for visiting movie show page and seeing its details and buttons to create viewing party and return to discover

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -6,22 +6,22 @@ class MovieFacade
 
   def movies
     if @search_param == "top_rated"
-      conn = Faraday.new(url: "https://api.themoviedb.org/3/discover/movie") do |f|
+      conn = Faraday.new(url: "https://api.themoviedb.org") do |f|
         f.params["api_key"] = Rails.application.credentials.tmdb[:key]
       end
 
-      response = conn.get("?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc")
+      response = conn.get("/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc")
       
       json = JSON.parse(response.body, symbolize_names: true)
       @movies = json[:results].map do |movie_data|
         Movie.new(movie_data)
       end
     else
-      conn = Faraday.new(url: "https://api.themoviedb.org/3/search/movie") do |f|
+      conn = Faraday.new(url: "https://api.themoviedb.org") do |f|
         f.params["api_key"] = Rails.application.credentials.tmdb[:key]
       end
 
-      response = conn.get("?query=#{@search_param}&include_adult=false&include_video=false&language=en-US&page=1")
+      response = conn.get("/3/search/movie?query=#{@search_param}&include_adult=false&include_video=false&language=en-US&page=1")
 
       json = JSON.parse(response.body, symbolize_names: true)
       @movies = json[:results].map do |movie_data|
--- spec/features/users/movies/show_spec.rb
+++ spec/features/users/movies/show_spec.rb
@@ -22,12 +22,17 @@ RSpec.describe 'Movies Show Page', type: :feature do
       # - Vote Average of the movie
       expect(page).to have_content(7.0)
       # - Runtime in hours & minutes
+      expect(page).to have_content("Runtime: 1 hour 34 minutes")
       # - Genre(s) associated to movie
+      expect(page).to have_content("Genre(s): Action, Adventure, Animation, Comedy, Family")
       # - Summary description
       expect(page).to have_content("Po is gearing up to become the spiritual leader of his Valley of Peace, but also needs someone to take his place as Dragon Warrior. As such, he will train a new kung fu practitioner for the spot and will encounter a villain called the Chameleon who conjures villains from the past.")
       # - List the first 10 cast members (characters & actress/actors)
+      expect(page).to have_content("Cast: Po (voice) - Jack Black, Zhen (voice) - Awkwafina, Li (voice) - Bryan Cranston, The Chameleon (voice) - Viola Davis, Shifu (voice) - Dustin Hoffman, Mr. Ping (voice) - James Hong, Tai Lung (voice) - Ian McShane, Han (voice) - Ke Huy Quan, Fish (voice) - Ronny Chieng, Granny Boar (voice) - Lori Tan Chinn")
       # - Count of total reviews
-      # - Each review's author and information      
+      expect(page).to have_content("Total Reviews: 1")
+      # - Each review's author and information
+      expect(page).to have_content("Reviewers:\nAuthor: Chris Swain\nUser Name: ChrisSwain")      
     end
   end
 end
