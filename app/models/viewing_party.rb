05c5ed579f447f9d7966d69e036b4bc907cd5beb
Jack Sweeney <jackcsweeney@gmail.com>
2024-03-21 15:21:07 -0700

    feat: Add validations for attributes of ViewingParty

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -1,8 +1,22 @@
 class ViewingParty < ApplicationRecord
-   has_many :user_parties
-   has_many :users, through: :user_parties
+  has_many :user_parties
+  has_many :users, through: :user_parties
 
-   def find_host
-      users.where("user_parties.host = true").first
-   end
+  validate :duration_cannot_be_shorter_than_movie
+
+  validates :start_time, presence: true
+  validates :date, presence: true
+  validates :duration, presence: true
+  validates :movie_id, presence: true
+  validates :movie_duration, presence: true, numericality: true
+
+  def find_host
+    users.where("user_parties.host = true").first
+  end
+
+  def duration_cannot_be_shorter_than_movie
+    if duration.present? && duration < movie_duration
+      errors.add(:duration, "cannot be shorter than movie runtime")
+    end
+  end
 end
--- spec/models/viewing_party_spec.rb
+++ spec/models/viewing_party_spec.rb
@@ -2,16 +2,26 @@ require 'rails_helper'
 
 RSpec.describe ViewingParty, type: :model do
   before(:each) do
-      @user_1 = User.create!(name: 'Sam', email: 'sam@email.com')
-      @user_2 = User.create!(name: 'Tommy', email: 'tommy@email.com')
-      @party = ViewingParty.create!(date: "2023-12-01", start_time: "07:25", duration: 175)
-      UserParty.create!(user_id: @user_1.id, viewing_party_id: @party.id, host: true)
-      UserParty.create!(user_id: @user_2.id, viewing_party_id: @party.id, host: false)
+    @user_1 = User.create!(name: 'Sam', email: 'sam@email.com')
+    @user_2 = User.create!(name: 'Tommy', email: 'tommy@email.com')
+    @party = ViewingParty.create!(date: "2023-12-01", start_time: "07:25", duration: 175, movie_duration: 132, movie_id: 1)
+  
+    UserParty.create!(user_id: @user_1.id, viewing_party_id: @party.id, host: true)
+    UserParty.create!(user_id: @user_2.id, viewing_party_id: @party.id, host: false)
+  end
+
+  describe '#validations' do
+  it { should validate_presence_of(:start_time) }
+  it { should validate_presence_of(:date) }
+  it { should validate_presence_of(:duration) }
+  it { should validate_presence_of(:movie_id) }
+  it { should validate_presence_of(:movie_duration) }
+  it { should validate_numericality_of(:movie_duration) }
   end
   
   describe 'relationships' do
-      it { should have_many :user_parties }
-      it { should have_many(:users).through(:user_parties) }
+    it { should have_many :user_parties }
+    it { should have_many(:users).through(:user_parties) }
   end
 
   describe "instance methods" do
