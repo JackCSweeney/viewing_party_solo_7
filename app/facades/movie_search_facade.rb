399822215b91e4d4a09d5b885a292d9f152ebb0b
Jack Sweeney <jackcsweeney@gmail.com>
2024-03-20 18:57:11 -0700

    refactor: Utilize MovieSearchService in MovieSearchFacade to abstract API call logic

--- app/facades/movie_search_facade.rb
+++ app/facades/movie_search_facade.rb
@@ -5,25 +5,17 @@ class MovieSearchFacade
   end
 
   def movies
-    if @search_param == "top_rated"
-      conn = Faraday.new(url: "https://api.themoviedb.org") do |f|
-        f.params["api_key"] = Rails.application.credentials.tmdb[:key]
-      end
+    service = MovieSearchService.new
 
-      response = conn.get("/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc")
-      
-      json = JSON.parse(response.body, symbolize_names: true)
+    if @search_param == "top_rated"
+      json = service.top_rated_movies
+
       @movies = json[:results].map do |movie_data|
         Movie.new(movie_data)
       end
     else
-      conn = Faraday.new(url: "https://api.themoviedb.org") do |f|
-        f.params["api_key"] = Rails.application.credentials.tmdb[:key]
-      end
+      json = service.movies_by_title(@search_param)
 
-      response = conn.get("/3/search/movie?query=#{@search_param}&include_adult=false&include_video=false&language=en-US&page=1")
-
-      json = JSON.parse(response.body, symbolize_names: true)
       @movies = json[:results].map do |movie_data|
         Movie.new(movie_data)
       end
