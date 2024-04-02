89641e528abba989d0cc17c29bc47b8250f6e944
Jack Sweeney <jackcsweeney@gmail.com>
2024-04-02 14:54:41 -0700

    feat: Ability to add location when logging in and see that location on the landing page and see that location prefilled when logging back in after logging out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
+      cookies.encrypted[:location] = params[:location]
       flash[:success] = "Welcome, #{user.name}"
       redirect_to user_path(user)
     else
